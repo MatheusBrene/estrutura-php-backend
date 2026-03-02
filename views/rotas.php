a1fe0f7f36021bc0aac12e2bc6f02521e6f00438
maercioferraz81 <aulaskodular2024@gmail.com>
2026-03-02 11:22:29 -0300

    base estrutura

---
+++ views/rotas.php
@@ -0,0 +1,7 @@
+ <?php
+    $paginasPermitidas = [
+        'index' => 'pagina-inicial.php',
+        'home' => 'pagina-inicial.php',
+        'categorias' => 'categoria/consultar.php',
+        'add/categoria' => 'categoria/salvar.php',
+    ];
