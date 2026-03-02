084b6d90ae5aea0028e1249f65cdc5e577b2a407
MatheusBrene <matheus.brene@etec.sp.gov.br>
2026-03-02 14:06:34 -0300

    Atividade 02/03 PWIII

--- views/rotas.php
+++ views/rotas.php
@@ -4,4 +4,8 @@
         'home' => 'pagina-inicial.php',
         'categorias' => 'categoria/consultar.php',
         'add/categoria' => 'categoria/salvar.php',
+        'fornecedor' => 'fornecedor/consultar.php',
+        'add/fornecedor' => 'fornecedor/salvar.php',
+        'clientes' => 'clientes/consultar.php',
+        'add/clientes' => 'clientes/salvar.php',
     ];
